2c36d45f63c7ac8086bab3d0a56efc04d15adbe5
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-02-17 01:06:35 +0330

    package01_variables

---
+++ src/package01_variables/MainClass.java
@@ -0,0 +1,24 @@
+package package01_variables;
+
+public class MainClass {
+    public static void main(String[] args) {
+        int number = 2;
+        number = 15 / 2;
+        System.out.println(number);
+        number = 1;
+        char ch = 'a';
+        ch = '1';
+        ch = '#';
+
+        byte b = 12;
+        short s = 1233;
+
+        float f = 3.14F;
+        double d = 127343.5342D;
+
+        boolean bool = true;
+
+        System.out.println(15 % 2);
+        System.out.println(1 + 2 * 3);
+    }
+}
